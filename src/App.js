e8447a6be5b89204d86e48ad7217e5b48e3b770e
Dan Proctor <dproc96@gmail.com>
2020-11-22 13:20:45 -0500

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
+import CalendarTemplate from './components/Calendar';
 
 function App() {
+  const [availability, setAvailability] = useState([])
+  const Calendar = CalendarTemplate({
+    availability,
+    setAvailability,
+  });
   return (
     <div>
-
+      <Calendar />
     </div>
   );
 }
